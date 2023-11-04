b4d90669c41901ff1c6641a6b2f6552cea57264c
Hasibul Hasan <hhasan.cse@gmail.com>
2023-11-05 01:21:38 +0600

    message2

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const a = 6;
-console.log(a);
+// const a = 6;
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
